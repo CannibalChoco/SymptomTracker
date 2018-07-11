622d84ef0549deb4147656601da808d0c8eb8af1
CannibalChoco <fudgeblimp@gmail.com>
2018-07-11 09:38:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/symptomtracker/TodayActivity.java
@@ -0,0 +1,44 @@
+package com.example.user.symptomtracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TodayActivity extends AppCompatActivity {
+
+    private TextView textMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_today:
+                    textMessage.setText("TODO: Today");
+                    return true;
+                case R.id.navigation_overview:
+                    textMessage.setText("TODO: Overview");
+                    return true;
+                case R.id.navigation_resolved:
+                    textMessage.setText("TODO: Resolved");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today);
+
+        textMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
