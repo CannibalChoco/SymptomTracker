db1f658d4d62eaf5446f8c8be86c948f6a55bc0e
CannibalChoco <fudgeblimp@gmail.com>
2018-07-31 14:06:38 +0300

    feat: Add widget, launch different fragments from widget. Has bug
    Has bug: launch app -> home -> launch fragment from widget -> back =
    previous activity underneath; if singleTop launch mode added, right
    fragment not launched, unless back clicked before

---
+++ app/src/main/java/com/example/user/symptomtracker/SymptomWidgetProvider.java
@@ -0,0 +1,84 @@
+package com.example.user.symptomtracker;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.user.symptomtracker.ui.AddSymptomActivity;
+import com.example.user.symptomtracker.ui.MainActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class SymptomWidgetProvider extends AppWidgetProvider {
+
+    public static final String WIDGET_KEY_FRAGMENT_ID = "widgetFragmentId";
+    public static final int WIDGET_TARGET_FRAGMENT_TODAY = 0;
+    public static final int WIDGET_TARGET_FRAGMENT_OVERVIEW = 1;
+    public static final int WIDGET_TARGET_ACTIVITY_NEW = 2;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.symptom_widget);
+
+        //  construct intent to launch overview fragment on "overview" click
+        PendingIntent overviewPendingIntent = getOverviewPendingIntent(context);
+        views.setOnClickPendingIntent(R.id.widget_overview_text, overviewPendingIntent);
+
+        // construct intent to launch today fragment on "today" click
+        PendingIntent todayPendingIntent = getTodayPendingIntent(context);
+        views.setOnClickPendingIntent(R.id.widget_today_text, todayPendingIntent);
+
+        // construct intent to launch add new activity
+        PendingIntent addSymptomPendingIntent = getAddSymptomPendingIntent(context);
+        views.setOnClickPendingIntent(R.id.widget_add_symptom, addSymptomPendingIntent);
+
+        // TODO: construct intent to launch detail view on item click
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private static PendingIntent getTodayPendingIntent(Context context) {
+        Intent todayIntent = new Intent(context, MainActivity.class);
+        todayIntent.putExtra(WIDGET_KEY_FRAGMENT_ID, WIDGET_TARGET_FRAGMENT_TODAY);
+        return PendingIntent.getActivity(context, 0,
+                todayIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private static PendingIntent getOverviewPendingIntent(Context context) {
+        Intent overviewIntent = new Intent(context, MainActivity.class);
+        overviewIntent.putExtra(WIDGET_KEY_FRAGMENT_ID, WIDGET_TARGET_FRAGMENT_OVERVIEW);
+        return PendingIntent.getActivity(context, 1,
+                overviewIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private static PendingIntent getAddSymptomPendingIntent(Context context) {
+        Intent intent = new Intent(context, AddSymptomActivity.class);
+        return PendingIntent.getActivity(context, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
--- app/src/main/java/com/example/user/symptomtracker/ui/MainActivity.java
+++ app/src/main/java/com/example/user/symptomtracker/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.user.symptomtracker.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 
@@ -17,6 +18,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.example.user.symptomtracker.SymptomWidgetProvider.WIDGET_KEY_FRAGMENT_ID;
+import static com.example.user.symptomtracker.SymptomWidgetProvider.WIDGET_TARGET_FRAGMENT_OVERVIEW;
+
 /**
  * Set up and manage navigation between destinations by Fragment transactions through
  * bottom navigation
@@ -24,6 +28,11 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     public static final String DUMMY_AD_ID = "ca-app-pub-3940256099942544~3347511713";
+    private static final int DEFAULT_FRAGMENT_ID = WIDGET_TARGET_FRAGMENT_OVERVIEW;
+    private static final int FRAGMENT_ADD = 99;
+    private static final int FRAGMENT_REPLACE = 88;
+
+    private FragmentManager fm;
 
     @BindView(R.id.navigation)
     BottomNavigationView navigationView;
@@ -34,20 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = item -> {
-                switch (item.getItemId()) {
-                    case R.id.navigation_today:
-                        fragmentReplaceWithToday();
-                        return true;
-                    case R.id.navigation_overview:
-                        fragmentReplaceWithOverview();
-                        return true;
-                    case R.id.navigation_resolved:
-                        fragmentReplaceWithResolved();
-                        return true;
-                }
+        switch (item.getItemId()) {
+            case R.id.navigation_today:
+                setTodayFragment(FRAGMENT_REPLACE);
+                return true;
+            case R.id.navigation_overview:
+                setOverviewFragment(FRAGMENT_REPLACE);
+                return true;
+            case R.id.navigation_resolved:
+                fragmentReplaceWithResolved();
+                return true;
+        }
 
-                return false;
-            };
+        return false;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,61 +64,116 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        fm = getSupportFragmentManager();
+        int fragmentId;
+        Intent intent = getIntent();
+
+        //setOverviewFragment(FRAGMENT_ADD);
+
+        // TODO: fix backstack bug
         if (savedInstanceState == null){
-            addDefaultFragmentToContainer();
-            navigationView.setSelectedItemId(R.id.navigation_overview);
+            // get id for which fragment to display
+            if (intent != null && intent.hasExtra(WIDGET_KEY_FRAGMENT_ID)) {
+                fragmentId = intent.getIntExtra(WIDGET_KEY_FRAGMENT_ID, DEFAULT_FRAGMENT_ID);
+            } else {
+                fragmentId = DEFAULT_FRAGMENT_ID;
+            }
+        } else {
+            fragmentId = DEFAULT_FRAGMENT_ID;
         }
+
+        // get id for which fragment to display
+        if (intent != null && intent.hasExtra(WIDGET_KEY_FRAGMENT_ID)) {
+            fragmentId = intent.getIntExtra(WIDGET_KEY_FRAGMENT_ID, DEFAULT_FRAGMENT_ID);
+        } else {
+            fragmentId = DEFAULT_FRAGMENT_ID;
+        }
+
+        int action;
+
+        // decide whether to add or replace fragment
+        if (savedInstanceState == null && !intent.hasExtra(WIDGET_KEY_FRAGMENT_ID)){
+            action = FRAGMENT_ADD;
+        } else {
+            action = FRAGMENT_REPLACE;
+        }
+
+
+        int backStack;
+        // display fragment
+        if (fragmentId == 0) {
+            navigationView.setSelectedItemId(R.id.navigation_today);
+            setTodayFragment(action);
+        } else {
+            navigationView.setSelectedItemId(R.id.navigation_overview);
+            setOverviewFragment(action);
+        }
+
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         // add Ads
-        MobileAds.initialize(this, DUMMY_AD_ID);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        bannerAd.loadAd(adRequest);
-
+        setupAds();
         // add Firebase Analytics
-        FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+        setupFirebaseAnalytics();
 
         JobServiceUtils.scheduleCheckUnresolvedSymptoms(this);
     }
 
+    private void setupFirebaseAnalytics() {
+        FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+    }
+
+    private void setupAds() {
+        MobileAds.initialize(this, DUMMY_AD_ID);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        bannerAd.loadAd(adRequest);
+    }
+
     @OnClick(R.id.fab_add_new)
-    public void addNewSymptom(){
+    public void addNewSymptom() {
         Intent intent = new Intent(MainActivity.this, AddSymptomActivity.class);
         startActivity(intent);
     }
 
     /**
-     * Add the OverviewFragment as the default to container as the views are first initialized
-     */
-    private void addDefaultFragmentToContainer() {
-        OverviewFragment overviewFragment = new OverviewFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.dest_fragment_container, overviewFragment)
-                .commit();
-    }
-
-    /**
      * Replace currently displayed fragment with OverviewFragment
      */
-    private void fragmentReplaceWithOverview() {
+    private void setOverviewFragment(int action) {
         OverviewFragment overviewFragment = new OverviewFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.dest_fragment_container, overviewFragment)
-                .commit();
+        int count = fm.getBackStackEntryCount();
+        if (action == FRAGMENT_ADD){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.dest_fragment_container, overviewFragment)
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.dest_fragment_container, overviewFragment)
+                    .commit();
+        }
+        count = fm.getBackStackEntryCount();
     }
 
     /**
      * Replace currently displayed fragment with TodayFragment
      */
-    private void fragmentReplaceWithToday() {
+    private void setTodayFragment(int action) {
         TodayFragment todayFragment = new TodayFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.dest_fragment_container, todayFragment)
-                .commit();
+        int count = fm.getBackStackEntryCount();
+        if (action == FRAGMENT_ADD){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.dest_fragment_container, todayFragment)
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.dest_fragment_container, todayFragment)
+                    .commit();
+        }
+        count = fm.getBackStackEntryCount();
     }
 
     /**
@@ -122,5 +186,4 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.dest_fragment_container, resolvedFragment)
                 .commit();
     }
-
 }
